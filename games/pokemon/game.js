5c7fe361c7556d49a0c480574c6d3f4ba3445298
Simon Uhrmann <simon.uhrmann@gmail.com>
2016-03-17 15:18:46 +0100

    Finished attack menu (incl. arrow-Input).  TODO: select-menu, fight-logic...

--- games/pokemon/game.js
+++ games/pokemon/game.js
@@ -31,49 +31,59 @@ JackDanger.PokemonVadammt = function () {
 };
 
 //hier musst du deine Eintragungen vornhemen.
-addMyGame("pokemon",
-    "JackDanger Pokemon",
-    "Vadammt",
-    "Pokemon - nur ohne Pokemon.",
-    "Steuerung", //Steuerkreuz
-    "[A] Auswahl", //Jump button belegung
-    "[B] Zurück", //Shoot button belegung
+addMyGame("pokemon",    // ID
+    "JackDanger Pokemon",           // Anzeige-Name
+    "Vadammt",          // Autor
+    "Pokemon - nur ohne Pokemon.",  // Beschreibung
+    "Steuerung",        //Steuerkreuz
+    "[A] Auswahl",      //Jump button belegung
+    "[B] Zurück",       //Shoot button belegung
     JackDanger.PokemonVadammt);
 
 
 JackDanger.PokemonVadammt.prototype.init = function () {
     logInfo("init Game");
     addLoadingScreen(this, true);//nicht anfassen
-}
+};
 
 JackDanger.PokemonVadammt.prototype.preload = function () {
     this.load.path = 'games/' + currentGameData.id + '/assets/';//nicht anfassen
 
-    //füge hie rein was du alles laden musst.
+    //füge hier ein was du alles laden musst.
     this.load.atlas("pokemon");
-}
+};
 
 //wird nach dem laden gestartet
 JackDanger.PokemonVadammt.prototype.create = function () {
     Pad.init();//nicht anfassen
-}
+
+    //  Set Background color
+    game.stage.backgroundColor = "#EEEEEE";
+};
 
 JackDanger.PokemonVadammt.prototype.mycreate = function () {
 
     this.addStuff();
-}
 
+    // Create Menu
+    var menuWidth = game.width * 0.5;
+    var menuHeight = game.height * 0.25;
+    this.createAttackMenu(game.width - menuWidth, game.height - menuHeight, menuWidth, menuHeight);
+    // Select a menu item
+    this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
+};
 
 //wird jeden Frame aufgerufen
 JackDanger.PokemonVadammt.prototype.update = function () {
     var dt = this.time.physicsElapsedMS * 0.001;
 
     this.playerControlls(dt);
-    this.updateBall(dt);
-    this.bounding();
-    this.collision();
-    this.updateTime(dt);
-}
+    /*    this.updateBall(dt);
+     this.bounding();
+     this.collision();
+     this.updateTime(dt);
+     */
+};
 
 /////////////////////////////////////////////////////////
 // Zeug das zum Spiel gehört, das kannst du alles /////// 
@@ -93,135 +103,143 @@ MenuItemPositions = {
     UPPER_RIGHT: 1,
     LOWER_LEFT: 2,
     LOWER_RIGHT: 3
-}
+};
 
-var SELECTED_INDICATOR = "-";
+var SELECTED_INDICATOR = "- ";
 
 JackDanger.PokemonVadammt.prototype.addStuff = function (dt) {
     // Player
-    this.player = this.add.sprite(20, 50, "pokemon", "face");
+    this.player = this.add.sprite(game.width * 0.25, 350, "pokemon", "face");
 
-    // Ball
-    this.ball = this.add.sprite(600, Math.random() * 300, "pokemon", "ball");
+    // Enemy
+    this.ball = this.add.sprite(game.width * 0.75, 50, "pokemon", "ball");
     this.ball.dir = {x: 1, y: 1};
 
     // Time Text
-    this.timeText = game.add.bitmapText(game.width / 2, 20, "testfont", "", 30);
+    this.timeText = game.add.bitmapText(game.width / 2, 20, "testfont", "TEST", 30);
     this.timeText.anchor.set(0.5);
-
-    // Create Menu
-    var menuWidth = game.width * 0.5;
-    var menuHeight = game.height * 0.25;
-    this.createTestMenu(game.width - menuWidth, game.height - menuHeight, menuWidth, menuHeight);
-    this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
 
     // Init values
     this.fightState = FightState.INIT;
-    this.selected = MenuItemPositions.UPPER_LEFT;
-}
+    this.selectedItemPosition = MenuItemPositions.UPPER_LEFT;
+};
 
-JackDanger.PokemonVadammt.prototype.createTestMenu = function (xPos, yPos, width, height, fontSize) {
+JackDanger.PokemonVadammt.prototype.createAttackMenu = function (xPos, yPos, width, height, fontSize) {
     if (isNaN(fontSize)) fontSize = 20;
 
     var yOffset = height * 0.25;
 
     // Add Text
     // 1st Row
-    var menu1Str = SELECTED_INDICATOR + "  Menu Item 1";
+    var menu1Str = "Menu Item 1";
     this.menu1 = game.add.bitmapText(xPos, yPos + yOffset, "testfont", menu1Str, fontSize);
     this.menu1.anchor.x = 0;
     this.menu1.anchor.y = 0.5;
+    this.menu1Selector = game.add.bitmapText(xPos, yPos + yOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu1Selector.anchor.x = 1;
+    this.menu1Selector.anchor.y = 0.5;
 
-    var menu2Str = "   Menu Item 2";
+    var menu2Str = "Menu Item 2";
     this.menu2 = game.add.bitmapText(xPos + width * 0.5, yPos + yOffset, "testfont", menu2Str, fontSize);
     this.menu2.anchor.x = 0;
     this.menu2.anchor.y = 0.5;
+    this.menu2Selector = game.add.bitmapText(xPos + width * 0.5, yPos + yOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu2Selector.anchor.x = 1;
+    this.menu2Selector.anchor.y = 0.5;
 
     // 2nd Row...
     yOffset *= 3;
-    var menu3Str = "   Menu Item 3";
+    var menu3Str = "Menu Item 3";
     this.menu3 = game.add.bitmapText(xPos, yPos + yOffset, "testfont", menu3Str, fontSize);
     this.menu3.anchor.x = 0;
     this.menu3.anchor.y = 0.5;
+    this.menu3Selector = game.add.bitmapText(xPos, yPos + yOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu3Selector.anchor.x = 1;
+    this.menu3Selector.anchor.y = 0.5;
 
-    var menu4Str = "   Menu Item 4";
+    var menu4Str = "Menu Item 4";
     this.menu4 = game.add.bitmapText(xPos + width * 0.5, yPos + yOffset, "testfont", menu4Str, fontSize);
     this.menu4.anchor.x = 0;
     this.menu4.anchor.y = 0.5;
-}
+    this.menu4Selector = game.add.bitmapText(xPos + width * 0.5, yPos + yOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu4Selector.anchor.x = 1;
+    this.menu4Selector.anchor.y = 0.5;
+};
 
 JackDanger.PokemonVadammt.prototype.selectMenuItem = function (menuItemPos) {
-    this.menu1.text = " " + this.menu1.text.substr(1);
-    this.menu2.text = " " + this.menu2.text.substr(1);
-    this.menu3.text = " " + this.menu3.text.substr(1);
-    this.menu4.text = " " + this.menu4.text.substr(1);
+    this.menu1Selector.text = "";
+    this.menu2Selector.text = "";
+    this.menu3Selector.text = "";
+    this.menu4Selector.text = "";
 
-    this.selected = this.findMenuItem(menuItemPos);
-    this.selected.text = SELECTED_INDICATOR + this.selected.text.substr(1);
-}
+    this.selectedItemPosition = menuItemPos;
+    var selectedSelector = this.findMenuItemSelector(menuItemPos);
+    selectedSelector.text = SELECTED_INDICATOR;
+};
 
-JackDanger.PokemonVadammt.prototype.findMenuItem = function (menuItemPos) {
+JackDanger.PokemonVadammt.prototype.findMenuItemSelector = function (menuItemPos) {
     switch (menuItemPos) {
         case MenuItemPositions.UPPER_LEFT:
-            return this.menu1;
+            return this.menu1Selector;
             break;
         case MenuItemPositions.UPPER_RIGHT:
-            return this.menu2;
+            return this.menu2Selector;
             break;
         case MenuItemPositions.LOWER_LEFT:
-            return this.menu3;
+            return this.menu3Selector;
             break;
         case MenuItemPositions.LOWER_RIGHT:
-            return this.menu4;
+            return this.menu4Selector;
             break;
         default:
             throw new Error("no action for " + menuItem + " defined.");
     }
-}
-
-JackDanger.PokemonVadammt.prototype.updateTime = function (dt) {
-    this.timeLeft -= dt;
-    this.timeText.setText("noch " + this.timeLeft.toFixed(1) + " Sekunden durchhalten!");
-
-    if (this.timeLeft <= 0) onVictory();
-}
-
-JackDanger.PokemonVadammt.prototype.updateBall = function (dt) {
-    this.ball.x += this.ball.dir.x * dt * this.ball.speed * 1.1;
-    this.ball.y += this.ball.dir.y * dt * this.ball.speed;
-
-    this.ball.speed += 30 * dt;
-
-    if (this.ball.x < 0) this.ball.dir.x *= -1;
-    if (this.ball.x > (this.game.width - this.ball.width)) this.ball.dir.x *= -1;
-    if (this.ball.y < 0) this.ball.dir.y *= -1;
-    if (this.ball.y > (this.game.height - this.ball.height)) this.ball.dir.y *= -1;
-}
+};
 
 JackDanger.PokemonVadammt.prototype.playerControlls = function (dt) {
-    if (Pad.justDown(Pad.LEFT)) {
-        // TODO: SMI2 + LEFT -> SMI1
-        this.player.x -= this.speed * dt;
-        isMoved = true;
-    }
 
-    if (Pad.isDown(Pad.RIGHT)) {
-        this.player.x += this.speed * dt;
-        isMoved = true;
-    }
-
-    if (Pad.isDown(Pad.UP)) {
-        this.player.y -= this.speed * dt;
-        isMoved = true;
-    }
-
-    if (Pad.isDown(Pad.DOWN)) {
-        this.player.y += this.speed * dt;
-        isMoved = true;
-    }
+    // Process the Arrow-Keys
+    this.changeSelection();
 
     this.speed += 100 * dt;
+};
+
+/**
+ *
+ * @returns {boolean} true if the selection changed.
+ */
+JackDanger.PokemonVadammt.prototype.changeSelection = function () {
+    switch (this.selectedItemPosition) {
+        case MenuItemPositions.UPPER_LEFT:
+            if(Pad.isDown(Pad.RIGHT))
+                this.selectMenuItem(MenuItemPositions.UPPER_RIGHT);
+            else if(Pad.isDown(Pad.DOWN))
+                this.selectMenuItem(MenuItemPositions.LOWER_LEFT);
+            break;
+
+        case MenuItemPositions.UPPER_RIGHT:
+            if(Pad.isDown(Pad.LEFT))
+                this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
+            else if(Pad.isDown(Pad.DOWN))
+                this.selectMenuItem(MenuItemPositions.LOWER_RIGHT);
+            break;
+
+        case MenuItemPositions.LOWER_LEFT:
+            if(Pad.isDown(Pad.RIGHT))
+                this.selectMenuItem(MenuItemPositions.LOWER_RIGHT);
+            else if(Pad.isDown(Pad.UP))
+                this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
+            break;
+
+        case MenuItemPositions.LOWER_RIGHT:
+            if(Pad.isDown(Pad.LEFT))
+                this.selectMenuItem(MenuItemPositions.LOWER_LEFT);
+            else if(Pad.isDown(Pad.UP))
+                this.selectMenuItem(MenuItemPositions.UPPER_RIGHT);
+            break;
+    }
 }
+
 
 JackDanger.PokemonVadammt.prototype.collision = function () {
     var difX = this.player.x - this.ball.x;
@@ -230,11 +248,11 @@ JackDanger.PokemonVadammt.prototype.collision = function () {
         //LOST
         onLose();
     }
-}
+};
 
 JackDanger.PokemonVadammt.prototype.bounding = function () {
     if (this.player.x < 0) this.player.x = 0;
     if (this.player.x > (this.game.width - this.player.width)) this.player.x = this.game.width - this.player.width;
     if (this.player.y < 0) this.player.y = 0;
     if (this.player.y > (this.game.height - this.player.height)) this.player.y = this.game.height - this.player.height;
-}
+};
