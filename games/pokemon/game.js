8c02fad6308b143a70542e7d6d7c61f8364ae440
Simon Uhrmann <simon.uhrmann@gmail.com>
2016-03-20 02:29:55 +0100

    Major refactoring: Attempt to store more stuff in package Pokemon...

--- games/pokemon/game.js
+++ games/pokemon/game.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  Hallo!
  Das hier ist deine Spielevorlage!
@@ -58,125 +59,70 @@ JackDanger.PokemonVadammt.prototype.create = function () {
     Pad.init();//nicht anfassen
 };
 
-Fighters = {
-    JackDanger: {
-        name: "Jack Danger",
-        maxHP: 100,
-        hp: 100,
-        attacks: [{name: "Punch", dmg: 10}, {name: "Platscher", dmg: 0}, {
-            name: "Roundhousekick",
-            dmg: 25
-        }, {name: "N/A", dmg: 0}]
-    },
-    Enemy1: {
-        name: "Fieser Fiesling",
-        maxHP: 50,
-        hp: 50,
-        attacks: [{name: "Beißen", dmg: 5}]
-    },
-    Enemy2: {
-        name: "Netter Fiesling",
-        maxHP: 100,
-        hp: 100,
-        attacks: [{name: "Beißen", dmg: 5}]
-    }
-};
-var enemies = [Fighters.Enemy1, Fighters.Enemy2];
-
-this.SELECTED_INDICATOR = "- ";
-
-this.FightStates = {
-    INIT: 0,
-    PLAYER_SELECT: 1,
-    PLAYER_ATTACK: 2,
-    ENEMY_SELECT: 3,
-    ENEMY_ATTACK: 4,
-    CHECK_GAME_OVER: 5
-};
-
-this.MenuItemPositions = {
-    UPPER_LEFT: 0,
-    UPPER_RIGHT: 1,
-    LOWER_LEFT: 2,
-    LOWER_RIGHT: 3
-};
-
 JackDanger.PokemonVadammt.prototype.mycreate = function () {
+
+    // Init all class-wide values
+    this.initValues();
+    this.initFighters();
 
     //  Set Background color
     game.stage.backgroundColor = "#EEEEEE";
 
     // Init values
-    this.gameState = FightStates.INIT;
-    this.selectedItemPosition = MenuItemPositions.UPPER_LEFT;
+    this.gameState = this.GameStates.INIT;
+    this.selectedItemPosition = this.MenuItemPositions.UPPER_LEFT;
 
+    // Add game content
     this.addStuff();
+
+    // Creation done -> switch gameState
+    this.initStateDone();
 };
 
-//wird jeden Frame aufgerufen
-JackDanger.PokemonVadammt.prototype.update = function () {
-    var deltaT = this.time.physicsElapsedMS * 0.001;
+JackDanger.PokemonVadammt.prototype.initValues = function () {
+    this.Fighters = {
+        JackDanger: {
+            name: "Jack Danger",
+            maxHP: 100,
+            attacks: [{name: "Punch", dmg: 10}, {name: "Platscher", dmg: 0}, {
+                name: "Roundhousekick",
+                dmg: 25
+            }, {name: "N/A", dmg: 0}]
+        },
+        Enemy1: {
+            name: "Fieser Fiesling",
+            maxHP: 50,
+            attacks: [{name: "Beißen", dmg: 5}]
+        },
+        Enemy2: {
+            name: "Netter Fiesling",
+            maxHP: 100,
+            attacks: [{name: "Beißen", dmg: 5}]
+        }
+    };
+    this.enemies = [this.Fighters.Enemy1, this.Fighters.Enemy2];
 
-    this.playerControlls(deltaT);
-    /*    this.updateBall(deltaT);
-     this.bounding();
-     this.collision();
-     this.updateTime(deltaT);
-     */
+    this.SELECTED_INDICATOR = "- ";
+
+    var tmpInitEnums = 0;
+    this.GameStates = {
+        INIT: tmpInitEnums++,
+        PLAYER_SELECT: tmpInitEnums++,
+        PLAYER_ATTACK: tmpInitEnums++,
+        ENEMY_SELECT: tmpInitEnums++,
+        ENEMY_ATTACK: tmpInitEnums++,
+        CHECK_GAME_OVER: tmpInitEnums++
+    };
+
+    this.attackMenu = {};
+    tmpInitEnums = 0;
+    this.MenuItemPositions = {
+        UPPER_LEFT: tmpInitEnums++,
+        UPPER_RIGHT: tmpInitEnums++,
+        LOWER_LEFT: tmpInitEnums++,
+        LOWER_RIGHT: tmpInitEnums++
+    };
 };
-
-JackDanger.PokemonVadammt.prototype.processStates = function () {
-    switch (this.gameState) {
-        case FightStates.INIT:
-            initState();
-            break;
-        case FightStates.PLAYER_SELECT:
-            playerSelectState();
-            break;
-        case FightStates.PLAYER_ATTACK:
-            playerAttackState();
-            break;
-        case FightStates.ENEMY_SELECT:
-            enemySelectState();
-            break;
-        case FightStates.ENEMY_ATTACK:
-            enemyAttackState();
-            break;
-        case FightStates.CHECK_GAME_OVER:
-            checkGameOverState();
-            break;
-    }
-
-    // TODO funktionen implementieren!
-    function initState() {
-
-    }
-
-    function playerSelectState() {
-
-    }
-
-    function playerAttackState() {
-
-    }
-
-    function enemySelectState() {
-
-    }
-
-    function enemyAttackState() {
-
-    }
-
-    function checkGameOverState() {
-
-    }
-}
-
-/////////////////////////////////////////////////////////
-// Zeug das zum Spiel gehört, das kannst du alles /////// 
-// Löschen oder ändern oder was weiß ich ////////////////
-/////////////////////////////////////////////////////////
 
 JackDanger.PokemonVadammt.prototype.addStuff = function (dt) {
     // Jack Danger sprite
@@ -195,19 +141,137 @@ JackDanger.PokemonVadammt.prototype.addStuff = function (dt) {
     var menuItemTexts = ["Attack 1", "Menu Item 2", "Menu Item 3", "Menu Item 3"];
     this.createAttackMenu(game.width - menuWidth, game.height - menuHeight, menuWidth, menuHeight, menuItemTexts);
     // Select a menu item
-    this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
+    this.selectMenuItem(this.MenuItemPositions.UPPER_LEFT);
 
     // Create Stats
     // Give all stats the same size
     var statsWidth = game.width * 0.5;
     var statsHeight = game.height * 0.25;
+
     // Create Jack Danger stats
-    this.createStats(0, 0, statsWidth, statsHeight, Fighters.Enemy1);
+    this.createStats(0, 0, statsWidth, statsHeight, this.fighterEnemy);
+
     // Create enemy stats
     var jdXPos = game.width * 0.5;
     var jdYPos = game.height * 0.5;
-    this.createStats(jdXPos, jdYPos, statsWidth, statsHeight, Fighters.JackDanger);
+    this.createStats(jdXPos, jdYPos, statsWidth, statsHeight, this.Fighters.JackDanger);
 
+};
+
+JackDanger.PokemonVadammt.prototype.initFighters = function () {
+    // Jack Danger
+    this.fighterJackDanger = this.Fighters.JackDanger;
+    this.fighterJackDanger.hp = this.fighterJackDanger.maxHP;
+
+    // 1st Enemy
+    this.fighterEnemy = this.Fighters.Enemy1;
+    this.fighterEnemy.hp = this.fighterEnemy.maxHP;
+};
+
+//wird jeden Frame aufgerufen
+JackDanger.PokemonVadammt.prototype.update = function () {
+    var deltaT = this.time.physicsElapsedMS * 0.001;
+
+    this.playerControlls(deltaT);
+    /*    this.updateBall(deltaT);
+     this.bounding();
+     this.collision();
+     this.updateTime(deltaT);
+     */
+};
+
+// TODO state-funktionen implementieren!
+JackDanger.PokemonVadammt.prototype.initStateDone = function () {
+    // Error checking
+    if (this.gameState != this.GameStates.INIT) {
+        throw new Error("Invalid state error. Currently not in state INIT.");
+    }
+
+    logInfo("State: INIT Done...");
+
+    // Switch to PLAYER_SELECT
+    this.playerSelectTransition();
+};
+
+JackDanger.PokemonVadammt.prototype.playerSelectTransition = function () {
+    this.gameState = GameStates.PLAYER_SELECT;
+    logInfo("State: PLAYER_SELECT...");
+};
+
+JackDanger.PokemonVadammt.prototype.playerSelectDone = function (chosenAttack) {
+    // Error checking
+    if (this.gameState != GameStates.PLAYER_SELECT) {
+        throw new Error("Invalid state error. Currently not in state PLAYER_SELECT.");
+    }
+
+    logInfo("State: PLAYER_SELECT Done...");
+
+    // Switch gameState
+    this.playerAttackTransition(chosenAttack);
+};
+
+JackDanger.PokemonVadammt.prototype.playerAttackTransition = function (chosenAttackIndex) {
+    this.gameState = this.GameStates.PLAYER_ATTACK;
+    logInfo("State: Player attacking...");
+
+    // TODO Attack...
+    this.processAttack(this.fighterJackDanger, this.fighterEnemy, chosenAttackIndex);
+
+    // Check if game is over, if not: Switch to ENEMY_SELECT
+    this.checkGameOverTransition(this.enemySelectTransition);
+};
+
+JackDanger.PokemonVadammt.prototype.processAttack = function (attacker, victim, attackIndex) {
+    var attackDmg = attacker.attacks[attackIndex];
+    victim.hp -= attackDmg;
+    logInfo(attacker.name +  " attacked " + victim.name + " - dealing " + attackDmg + " damage.");
+};
+
+JackDanger.PokemonVadammt.prototype.enemySelectTransition = function () {
+    this.gameState = GameStates.ENEMY_SELECT;
+    logInfo("State: Enemy selecting...");
+    // TODO Implement a better strategy...
+    var selectedAttackIndex = 0;
+
+    // Switch to ENEMY_ATTACK
+    this.enemyAttackTransition(selectedAttackIndex);
+};
+
+/**
+ *
+ * @param attackIndex The index of the selected attack in the fighter array (0 to 3).
+ */
+JackDanger.PokemonVadammt.prototype.enemyAttackTransition = function (attackIndex) {
+    this.gameState = this.GameStates.ENEMY_ATTACK;
+    logInfo("State: Enemy attacking...");
+
+    var attack = this.fighterEnemy.attacks[attackIndex];
+    logInfo("Enemy attacking with " + attack.name);
+
+    // Hit Jack Danger
+    JackDanger.hp -= attack.dmg;
+};
+
+/**
+ *
+ * @param nextStateTransition The function that switches to the next state if the game is not over.
+ */
+JackDanger.PokemonVadammt.prototype.checkGameOverTransition = function (nextStateTransition) {
+    logInfo("State: Check is game over...");
+
+    if (JackDanger.hp <= 0) {
+        logInfo("Game is over! You lose!");
+        onLose();
+    }
+    else if (fighterEnemy.hp <= 0) {
+        logInfo("Game is over! You win!");
+        onVictory();
+    }
+
+    // Run the
+    if (typeof nextStateTransition === "function") {
+        nextStateTransition();
+    }
 };
 
 JackDanger.PokemonVadammt.prototype.createAttackMenu = function (xPos, yPos, width, height, menuEntries, fontSize) {
@@ -231,36 +295,36 @@ JackDanger.PokemonVadammt.prototype.createAttackMenu = function (xPos, yPos, wid
 
     // Add Text
     // UPPER_LEFT
-    this.menu1 = game.add.bitmapText(newXPos + menuSelectorWidth, newYPos + textYOffset, "testfont", menuEntries[MenuItemPositions.UPPER_LEFT], fontSize);
+    this.menu1 = game.add.bitmapText(newXPos + menuSelectorWidth, newYPos + textYOffset, "testfont", menuEntries[this.MenuItemPositions.UPPER_LEFT], fontSize);
     this.menu1.anchor.x = 0;
     this.menu1.anchor.y = 0.5;
-    this.menu1Selector = game.add.bitmapText(newXPos, newYPos + textYOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu1Selector = game.add.bitmapText(newXPos, newYPos + textYOffset, "testfont", this.SELECTED_INDICATOR, fontSize);
     this.menu1Selector.anchor.x = 0;
     this.menu1Selector.anchor.y = 0.5;
 
     // UPPER_RIGHT
-    this.menu2 = game.add.bitmapText(newXPos + menuSelectorWidth + newWidth * 0.5, newYPos + textYOffset, "testfont", menuEntries[MenuItemPositions.UPPER_RIGHT], fontSize);
+    this.menu2 = game.add.bitmapText(newXPos + menuSelectorWidth + newWidth * 0.5, newYPos + textYOffset, "testfont", menuEntries[this.MenuItemPositions.UPPER_RIGHT], fontSize);
     this.menu2.anchor.x = 0;
     this.menu2.anchor.y = 0.5;
-    this.menu2Selector = game.add.bitmapText(newXPos + newWidth * 0.5, newYPos + textYOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu2Selector = game.add.bitmapText(newXPos + newWidth * 0.5, newYPos + textYOffset, "testfont", this.SELECTED_INDICATOR, fontSize);
     this.menu2Selector.anchor.x = 0;
     this.menu2Selector.anchor.y = 0.5;
 
     // 2nd Row...
     textYOffset *= 3;
     // LOWER_LEFT
-    this.menu3 = game.add.bitmapText(newXPos + menuSelectorWidth, newYPos + textYOffset, "testfont", menuEntries[MenuItemPositions.LOWER_LEFT], fontSize);
+    this.menu3 = game.add.bitmapText(newXPos + menuSelectorWidth, newYPos + textYOffset, "testfont", menuEntries[this.MenuItemPositions.LOWER_LEFT], fontSize);
     this.menu3.anchor.x = 0;
     this.menu3.anchor.y = 0.5;
-    this.menu3Selector = game.add.bitmapText(newXPos, newYPos + textYOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu3Selector = game.add.bitmapText(newXPos, newYPos + textYOffset, "testfont", this.SELECTED_INDICATOR, fontSize);
     this.menu3Selector.anchor.x = 0;
     this.menu3Selector.anchor.y = 0.5;
 
     // LOWER_RIGHT
-    this.menu4 = game.add.bitmapText(newXPos + menuSelectorWidth + newWidth * 0.5, newYPos + textYOffset, "testfont", menuEntries[MenuItemPositions.LOWER_RIGHT], fontSize);
+    this.menu4 = game.add.bitmapText(newXPos + menuSelectorWidth + newWidth * 0.5, newYPos + textYOffset, "testfont", menuEntries[this.MenuItemPositions.LOWER_RIGHT], fontSize);
     this.menu4.anchor.x = 0;
     this.menu4.anchor.y = 0.5;
-    this.menu4Selector = game.add.bitmapText(newXPos + newWidth * 0.5, newYPos + textYOffset, "testfont", SELECTED_INDICATOR, fontSize);
+    this.menu4Selector = game.add.bitmapText(newXPos + newWidth * 0.5, newYPos + textYOffset, "testfont", this.SELECTED_INDICATOR, fontSize);
     this.menu4Selector.anchor.x = 0;
     this.menu4Selector.anchor.y = 0.5;
 };
@@ -273,21 +337,21 @@ JackDanger.PokemonVadammt.prototype.selectMenuItem = function (menuItemPos) {
 
     this.selectedItemPosition = menuItemPos;
     var selectedSelector = this.findMenuItemSelector(menuItemPos);
-    selectedSelector.text = SELECTED_INDICATOR;
+    selectedSelector.text = this.SELECTED_INDICATOR;
 };
 
 JackDanger.PokemonVadammt.prototype.findMenuItemSelector = function (menuItemPos) {
     switch (menuItemPos) {
-        case MenuItemPositions.UPPER_LEFT:
+        case this.MenuItemPositions.UPPER_LEFT:
             return this.menu1Selector;
             break;
-        case MenuItemPositions.UPPER_RIGHT:
+        case this.MenuItemPositions.UPPER_RIGHT:
             return this.menu2Selector;
             break;
-        case MenuItemPositions.LOWER_LEFT:
+        case this.MenuItemPositions.LOWER_LEFT:
             return this.menu3Selector;
             break;
-        case MenuItemPositions.LOWER_RIGHT:
+        case this.MenuItemPositions.LOWER_RIGHT:
             return this.menu4Selector;
             break;
         default:
@@ -316,7 +380,7 @@ JackDanger.PokemonVadammt.prototype.createStats = function (xPos, yPos, width, h
     var indicatorYPos = yPos + fontSize * 2;
     var indicatorWidth = (width - (indicatorXPos - xPos)) * 0.9;
     var indicator = game.add.graphics(0, 0);
-    indicator.beginFill(this.indicatorColor(owner.hp / owner.maxHP));
+    indicator.beginFill(this.calcIndicatorColor(owner.hp / owner.maxHP));
     indicator.drawRect(indicatorXPos, indicatorYPos, indicatorWidth, fontSize * 1.75);
     indicator.endFill();
     var indicatorBorder = game.add.graphics(0, 0);
@@ -336,7 +400,7 @@ JackDanger.PokemonVadammt.prototype.createStats = function (xPos, yPos, width, h
     };
 };
 
-JackDanger.PokemonVadammt.prototype.indicatorColor = function (healthInPercent) {
+JackDanger.PokemonVadammt.prototype.calcIndicatorColor = function (healthInPercent) {
     // TODO: Green to red gradient (see http://phaser.io/docs/2.4.6/Phaser.Color.html#.HSVColorWheel and http://phaser.io/examples/v2/display/hsv-color-wheel).
 //    var colors = Phaser.Color.HSVColorWheel();
     return 0xDD0000;
@@ -344,45 +408,56 @@ JackDanger.PokemonVadammt.prototype.indicatorColor = function (healthInPercent) 
 
 JackDanger.PokemonVadammt.prototype.playerControlls = function (dt) {
 
-    // Process the Arrow-Keys
-    this.changeSelection();
+    // Only accept selection in the state PLAYER_SELECT
+    if (this.gameState == GameStates.PLAYER_SELECT) {
+        // Process the Arrow-Keys
+        changeAttackMenuSelection();
 
-    if (Pad.justDown(Pad.SHOOT)) {
-        // TODO Select an menu-item
-        logInfo("Selected " + this.selectedItemPosition.toString());
+        chooseAttack();
+    }
+
+    function changeAttackMenuSelection() {
+        switch (this.selectedItemPosition) {
+            case this.MenuItemPositions.UPPER_LEFT:
+                if (Pad.justDown(Pad.RIGHT))
+                    this.selectMenuItem(this.MenuItemPositions.UPPER_RIGHT);
+                else if (Pad.justDown(Pad.DOWN))
+                    this.selectMenuItem(this.MenuItemPositions.LOWER_LEFT);
+                break;
+
+            case MenuItemPositions.UPPER_RIGHT:
+                if (Pad.justDown(Pad.LEFT))
+                    this.selectMenuItem(this.MenuItemPositions.UPPER_LEFT);
+                else if (Pad.justDown(Pad.DOWN))
+                    this.selectMenuItem(this.MenuItemPositions.LOWER_RIGHT);
+                break;
+
+            case MenuItemPositions.LOWER_LEFT:
+                if (Pad.justDown(Pad.RIGHT))
+                    this.selectMenuItem(this.MenuItemPositions.LOWER_RIGHT);
+                else if (Pad.justDown(Pad.UP))
+                    this.selectMenuItem(this.MenuItemPositions.UPPER_LEFT);
+                break;
+
+            case MenuItemPositions.LOWER_RIGHT:
+                if (Pad.justDown(Pad.LEFT))
+                    this.selectMenuItem(this.MenuItemPositions.LOWER_LEFT);
+                else if (Pad.justDown(Pad.UP))
+                    this.selectMenuItem(this.MenuItemPositions.UPPER_RIGHT);
+                break;
+        }
+    }
+
+    function chooseAttack() {
+        if (Pad.justDown(Pad.SHOOT)) {
+            // TODO Select an menu-item
+            logInfo("Selected " + this.selectedItemPosition.toString());
+
+            // Selection is over -> Inform Switch-Machine
+            var selectedAttack = this.selectedItemPosition;
+            this.playerSelectDone(selectedAttack);
+        }
     }
 
     this.speed += 100 * dt;
-};
-
-JackDanger.PokemonVadammt.prototype.changeSelection = function () {
-    switch (this.selectedItemPosition) {
-        case MenuItemPositions.UPPER_LEFT:
-            if (Pad.justDown(Pad.RIGHT))
-                this.selectMenuItem(MenuItemPositions.UPPER_RIGHT);
-            else if (Pad.justDown(Pad.DOWN))
-                this.selectMenuItem(MenuItemPositions.LOWER_LEFT);
-            break;
-
-        case MenuItemPositions.UPPER_RIGHT:
-            if (Pad.justDown(Pad.LEFT))
-                this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
-            else if (Pad.justDown(Pad.DOWN))
-                this.selectMenuItem(MenuItemPositions.LOWER_RIGHT);
-            break;
-
-        case MenuItemPositions.LOWER_LEFT:
-            if (Pad.justDown(Pad.RIGHT))
-                this.selectMenuItem(MenuItemPositions.LOWER_RIGHT);
-            else if (Pad.justDown(Pad.UP))
-                this.selectMenuItem(MenuItemPositions.UPPER_LEFT);
-            break;
-
-        case MenuItemPositions.LOWER_RIGHT:
-            if (Pad.justDown(Pad.LEFT))
-                this.selectMenuItem(MenuItemPositions.LOWER_LEFT);
-            else if (Pad.justDown(Pad.UP))
-                this.selectMenuItem(MenuItemPositions.UPPER_RIGHT);
-            break;
-    }
 };
